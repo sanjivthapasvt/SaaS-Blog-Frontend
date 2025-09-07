165c30e9b8e7f3ac422ae03b6fe84a74ebd58f34
Sanjiv Thapa <thapasvt12@gmail.com>
2025-09-07 20:15:29 +0545

    Added axios in project

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react"
+import { useState } from "react";
+import axios from "axios";
+
+const baseUrl:string = "localhost:8000"
 
 export default function Home() {
   const [blogdata, setBlogData] = useState();
+  const getLatestBlogs = async () => {
+    axios.get(`${baseUrl}/api/blogs`)
+  };
 
-  
-  return (
-  <></>
-  )
+  return <></>;
 }
